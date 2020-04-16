7e54a875b91af20f1f3df659cdb221ff330848ce
NalaElRayes <54401398+NalaElRayes@users.noreply.github.com>
2020-04-16 15:01:47 +0200

    Changed the annotation on the controller class from @Restcontroller to controller, since the rest controller only takes json or string.

--- src/main/java/app/todo/TodoController.java
+++ src/main/java/app/todo/TodoController.java
@@ -1,14 +1,15 @@
 package app.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping(value = "/todo")
-public class Controller {
+public class TodoController {
 
     @Autowired
     private TodoService todoService;
