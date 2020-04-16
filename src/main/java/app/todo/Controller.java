1acc8abc3762053828cb803bdc59e79c76d97805
NalaElRayes <54401398+NalaElRayes@users.noreply.github.com>
2020-04-16 12:19:02 +0200

    Changed the updateActive method and added some notes.

--- src/main/java/app/todo/Controller.java
+++ src/main/java/app/todo/Controller.java
@@ -13,29 +13,32 @@ public class Controller {
     @Autowired
     private TodoService todoService;
 
-
+    //Creates a todo
     @PostMapping
     public Todo createTodo(@RequestParam Map<String, String> body){
          return todoService.createTodo(body.get("text"));
     }
 
+    /*returns a list of todos*/
     @GetMapping
     public List<Todo> getList(){
         return todoService.getAll();
 
     }
 
+    /*takes an id of an todo and deletes it. */
     @DeleteMapping("/{id}")
     public void deleteTodo(@PathVariable int id) {
         todoService.deleteTodo(id);
     }
 
+    /*Shos all actives todo in a list*/
     @GetMapping("/allActive")
     public List<Todo> getAllActive(){
         return todoService.getAllActive();
     }
 
-
+    /* Takes in a id of an todo and changes it to active or false, the user choose*/
     @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
     public void updateActive(@RequestBody Todo todo, @PathVariable int id) {
         todoService.updateActive(id, todo);
